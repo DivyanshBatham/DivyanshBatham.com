b44c47f68de1f4cda8d5d82943eb715b79db7213
DivyanshBatham <divyansh.sunita@gmail.com>
2022-05-11 01:58:20 +0530

    update

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -23,38 +23,48 @@ const config: GatsbyConfig = {
         },
       },
     },
-    // {
-    //   resolve: `gatsby-plugin-google-analytics`,
-    //   options: {
-    //     // The property ID; the tracking code won't be generated without it
-    //     // trackingId: "G-J0X8CLTB8J",
-    //     trackingId: "G-S2MJBBHT1Q",
-    //     // Defines where to place the tracking script - `true` in the head and `false` in the body
-    //     // head: false,
-    //     // // Setting this parameter is optional
-    //     // anonymize: true,
-    //     // // Setting this parameter is also optional
-    //     // respectDNT: true,
-    //     // // Avoids sending pageview hits from custom paths
-    //     // exclude: ["/preview/**", "/do-not-track/me/too/"],
-    //     // // Delays sending pageview hits on route update (in milliseconds)
-    //     // pageTransitionDelay: 0,
-    //     // // Enables Google Optimize using your container Id
-    //     // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
-    //     // // Enables Google Optimize Experiment ID
-    //     // experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
-    //     // // Set Variation ID. 0 for original 1,2,3....
-    //     // variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
-    //     // // Defers execution of google analytics script after page load
-    //     // defer: false,
-    //     // // Any additional optional fields
-    //     // sampleRate: 5,
-    //     // siteSpeedSampleRate: 10,
-    //     // cookieDomain: "example.com",
-    //     // // defaults to false
-    //     // enableWebVitalsTracking: true,
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        // trackingId: "G-J0X8CLTB8J",
+        trackingId: "G-S2MJBBHT1Q",
+
+        // Puts tracking script in the head instead of the body
+        head: true,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: [`/preview/**`, `/do-not-track/me/too/`],
+
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        // head: false,
+        // // Setting this parameter is optional
+        // anonymize: true,
+        // // Setting this parameter is also optional
+        // respectDNT: true,
+        // // Avoids sending pageview hits from custom paths
+        // exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // // Delays sending pageview hits on route update (in milliseconds)
+        // pageTransitionDelay: 0,
+        // // Enables Google Optimize using your container Id
+        // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+        // // Enables Google Optimize Experiment ID
+        // experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
+        // // Set Variation ID. 0 for original 1,2,3....
+        // variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
+        // // Defers execution of google analytics script after page load
+        // defer: false,
+        // // Any additional optional fields
+        // sampleRate: 5,
+        // siteSpeedSampleRate: 10,
+        // cookieDomain: "example.com",
+        // // defaults to false
+        // enableWebVitalsTracking: true,
+      },
+    },
     // {
     //   resolve: "gatsby-plugin-google-tagmanager",
     //   options: {
