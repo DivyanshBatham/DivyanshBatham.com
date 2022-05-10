8e4e2ac0aad189f3fc7f4cc2b281079abd30db48
DivyanshBatham <divyansh.sunita@gmail.com>
2022-05-11 01:21:23 +0530

    Added GA

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -11,6 +11,71 @@ const config: GatsbyConfig = {
   },
 
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "G-J0X8CLTB8J",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        // head: false,
+        // // Setting this parameter is optional
+        // anonymize: true,
+        // // Setting this parameter is also optional
+        // respectDNT: true,
+        // // Avoids sending pageview hits from custom paths
+        // exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // // Delays sending pageview hits on route update (in milliseconds)
+        // pageTransitionDelay: 0,
+        // // Enables Google Optimize using your container Id
+        // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+        // // Enables Google Optimize Experiment ID
+        // experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
+        // // Set Variation ID. 0 for original 1,2,3....
+        // variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
+        // // Defers execution of google analytics script after page load
+        // defer: false,
+        // // Any additional optional fields
+        // sampleRate: 5,
+        // siteSpeedSampleRate: 10,
+        // cookieDomain: "example.com",
+        // // defaults to false
+        // enableWebVitalsTracking: true,
+      },
+    },
+    // {
+    //   resolve: "gatsby-plugin-google-tagmanager",
+    //   options: {
+    //     id: "G-J0X8CLTB8J",
+
+    //     // Analytics ID: G-J0X8CLTB8J
+
+    //     // Include GTM in development.
+    //     //
+    //     // Defaults to false meaning GTM will only be loaded in production.
+    //     includeInDevelopment: true,
+
+    //     // datalayer to be set before GTM is loaded
+    //     // should be an object or a function that is executed in the browser
+    //     //
+    //     // Defaults to null
+    //     // defaultDataLayer: { platform: "gatsby" },
+
+    //     // Specify optional GTM environment details.
+    //     // gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
+    //     // gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
+    //     // dataLayerName: "YOUR_DATA_LAYER_NAME",
+
+    //     // Name of the event that is triggered
+    //     // on every Gatsby route change.
+    //     //
+    //     // Defaults to gatsby-route-change
+    //     // routeChangeEventName: "YOUR_ROUTE_CHANGE_EVENT_NAME",
+    //     // Defaults to false
+    //     // enableWebVitalsTracking: true,
+    //     // Defaults to https://www.googletagmanager.com
+    //     // selfHostedOrigin: "YOUR_SELF_HOSTED_ORIGIN",
+    //   },
+    // },
     "gatsby-plugin-image",
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
